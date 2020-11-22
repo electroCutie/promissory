c6293a866eb689e85a66f7249ab5560ebeefe305
Colette 🌩️‍💝 Kerr <Colette.m.y.Kerr@gmail.com>
2020-11-22 17:36:19 +0100

    clean up and add documentation, also roll both promisory types into one trait

--- src/basic_promissory.rs
+++ src/basic_promissory.rs
@@ -5,10 +5,16 @@ pub(crate) struct Promissory<T> {
     pub(crate) cond: Condvar,
 }
 
+/// Object which allows a one-shot fulfillment of the promissory
 pub struct Fulfiller<T>(pub(crate) Arc<Promissory<T>>);
-pub struct Awaiter<T>(Arc<Promissory<T>>);
 
-pub fn promissory<T>() -> (Fulfiller<T>, Awaiter<T>)
+pub trait Awaiter<T: Send> {
+    /// Attempt to retrive the computed value, blocking if nessesary
+    fn await_value(self) -> T;
+}
+
+/// Construct a Fullfiller / Awaiter pair
+pub fn promissory<T>() -> (Fulfiller<T>, BaseAwaiter<T>)
 where
     T: Send,
 {
@@ -17,24 +23,25 @@ where
         cond: Condvar::new(),
     });
 
-    (Fulfiller(p.clone()), Awaiter(p))
+    (Fulfiller(p.clone()), BaseAwaiter(p))
 }
 
 impl<T> Fulfiller<T>
 where
     T: Send,
 {
+    /// Consume the fulfiller and awake any waiters / mark the Promissory as fulfilled
     pub fn fulfill(self, t: T) {
         *(self.0.mutex.lock().expect("broken lock")) = Some(t);
         self.0.cond.notify_all() // All because we use this one impl for this and clone
     }
 }
 
-impl<T> Awaiter<T>
-where
-    T: Send,
-{
-    pub fn await_value(self) -> T {
+/// An await that cannot be cloned
+pub struct BaseAwaiter<T>(Arc<Promissory<T>>);
+
+impl<T: Send> Awaiter<T> for BaseAwaiter<T> {
+    fn await_value(self) -> T {
         let lock = self.0.mutex.lock().expect("broken lock");
         let cond = &self.0.cond;
         let mut lock = cond
@@ -49,6 +56,38 @@ where
     }
 }
 
+/// An Awaiter that can be cloned
+#[derive(Clone)]
+pub struct CloneAwaiter<T: Clone>(Arc<Promissory<T>>);
+
+/// Construct a Fullfiller / Awaiter pair where the Awaiter implements Clone
+pub fn cloneable_promissory<T: Clone>() -> (Fulfiller<T>, CloneAwaiter<T>)
+where
+    T: Send,
+{
+    let p = Arc::new(Promissory {
+        mutex: Mutex::new(None),
+        cond: Condvar::new(),
+    });
+
+    (Fulfiller(p.clone()), CloneAwaiter(p))
+}
+
+impl<T> Awaiter<T> for CloneAwaiter<T>
+where
+    T: Send + Clone,
+{
+    fn await_value(self) -> T {
+        let lock = self.0.mutex.lock().expect("broken lock");
+        let cond = &self.0.cond;
+        let lock = cond
+            .wait_while(lock, |o| o.is_none())
+            .expect("broken condition");
+
+        lock.clone().unwrap()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -59,5 +98,19 @@ mod tests {
         let (send, recv) = promissory();
         thread::spawn(move || send.fulfill(42));
         assert_eq!(42, recv.await_value());
+    }
+
+    #[test]
+    fn cloning() {
+        let (send, recv) = cloneable_promissory();
+
+        let recv_b = recv.clone();
+
+        thread::spawn(move || send.fulfill(42));
+
+        let joiner = thread::spawn(move || assert_eq!(42, recv_b.await_value()));
+
+        assert_eq!(42, recv.await_value());
+        joiner.join().expect("err in thread b");
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
-mod basic_promissory;
-mod cloneable_promissory;
+//! One-shot value exhange between threads. The consumer thread may await the producer thread
+//! Both the producer and consumer are single use, though there is a version of the consumer that may be cloned
+//! 
+//! # Example
+//! 
+//! ```
+//! let (send, recv) = promissory();
+//! thread::spawn(move || send.fulfill(42));
+//! assert_eq!(42, recv.await_value());
+//! ```
+//! 
 
-pub use basic_promissory::{promissory, Awaiter, Fulfiller};
-pub use cloneable_promissory::{cloneable_promissory, CloneAwaiter};
+
+mod basic_promissory;
+
+pub use crate::basic_promissory::{
+    cloneable_promissory, promissory, Awaiter, BaseAwaiter, CloneAwaiter, Fulfiller,
+};
