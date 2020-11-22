e9689e9c5de9e8ab27fd52cfc4a882fc746d552a
Colette 🌩️‍💝 Kerr <Colette.m.y.Kerr@gmail.com>
2020-11-22 17:45:55 +0100

    Fixed the example code in the doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 //! One-shot value exhange between threads. The consumer thread may await the producer thread
 //! Both the producer and consumer are single use, though there is a version of the consumer that may be cloned
-//! 
+//!
 //! # Example
-//! 
+//!
 //! ```
-//! let (send, recv) = promissory();
-//! thread::spawn(move || send.fulfill(42));
+//! use promissory::{promissory, Awaiter};
+//! let (send, recv) = promissory::promissory();
+//! std::thread::spawn(move || send.fulfill(42u32));
 //! assert_eq!(42, recv.await_value());
 //! ```
-//! 
-
+//!
 
 mod basic_promissory;
 
